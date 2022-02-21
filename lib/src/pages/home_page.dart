2dc1e7be2bd0b5d11c284270f6a090b81a7a5752
HildaPech <72896837+HildaPech@users.noreply.github.com>
2022-02-21 06:38:23 -0500

    video12_Utilidades - Obtener ícono por strings

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:componentes/src/providers/menu_provider.dart';
+import 'package:componentes/src/utils/icono_string_util.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -11,23 +12,35 @@ class HomePage extends StatelessWidget {
       body: _lista(),
     );
   }
-
   Widget _lista() {
-
-    print( menuProvider.opciones );
-
-    return ListView(
-      children: _listaItems(),
+    //menuProvider.cargarData();
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaItems(snapshot.data!),
+        );
+      },
     );
   }
-
-   List<Widget> _listaItems() {
-    return[
-      ListTile(title: Text('Hola Mundo')),
-      Divider(),
-      ListTile(title: Text('Hola Mundo')),
-      Divider(),
-      ListTile(title: Text('Hola Mundo')),
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+    data.forEach((opt) {
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        
+        leading: getIcon(opt['icon']),
+        trailing: Icon(
+          Icons.keyboard_arrow_right,
+          color: Colors.pinkAccent,
+        ),
+        onTap: () {},
+      );
+      opciones
+        ..add(widgetTemp)
+        ..add(Divider());
+    });
+    return opciones;
   }
 }
