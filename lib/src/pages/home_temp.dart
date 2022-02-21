1ef18cc6cf269569da567b2eef33dcfa32aff025
HildaPech <72896837+HildaPech@users.noreply.github.com>
2022-02-21 00:51:21 -0500

    video7_Método map de las listas y ListTile

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -11,7 +11,8 @@ class HomePageTemp extends StatelessWidget {
         title: Text('Componentes Temp'),
       ),
       body: ListView(
-        children: _crearItems()
+        //children: _crearItems()
+        children: _crearItemsCorta()
       ),
     );
   }
@@ -29,6 +30,22 @@ class HomePageTemp extends StatelessWidget {
     return lista;
   }
   List<Widget> _crearItemsCorta(){
+      return opciones.map((item){
+
+        return Column(
+          children: <Widget>[
+            ListTile(
+              title: Text(item + '!'),
+              subtitle: Text('Números'),
+              leading: Icon(Icons.account_balance_wallet),
+              trailing: Icon(Icons.keyboard_arrow_right),
+              onTap: (){ },
+      
+            ),
+            Divider()
+          ],
+        );
+      }).toList();
 
   }
 }
