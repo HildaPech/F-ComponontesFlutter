5e8c27caf394558ef55ac33b936af1527b10a51d
HildaPech <72896837+HildaPech@users.noreply.github.com>
2022-02-21 15:29:25 -0500

    video13_Navegar a una nueva pantalla en Flutter

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/providers/menu_provider.dart';
 import 'package:componentes/src/utils/icono_string_util.dart';
-import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -19,12 +20,12 @@ class HomePage extends StatelessWidget {
       initialData: [],
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _listaItems(snapshot.data!),
+          children: _listaItems(snapshot.data!, context),
         );
       },
     );
   }
-  List<Widget> _listaItems(List<dynamic> data) {
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
     data.forEach((opt) {
       final widgetTemp = ListTile(
@@ -35,7 +36,12 @@ class HomePage extends StatelessWidget {
           Icons.keyboard_arrow_right,
           color: Colors.pinkAccent,
         ),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(
+            builder: (context)=>AlertPage()    
+          );
+          Navigator.push(context, route);
+        },
       );
       opciones
         ..add(widgetTemp)
