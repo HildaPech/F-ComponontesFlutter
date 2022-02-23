5c50939518e8754c9b2b7bc8e9979724783365dd
HildaPech <72896837+HildaPech@users.noreply.github.com>
2022-02-22 21:27:49 -0500

    video30_Listview Builder

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:componentes/src/pages/slider_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/alert_page.dart';
@@ -6,6 +7,7 @@ import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/listview_page.dart';
 
 
 Map<String, WidgetBuilder> getApplicationRoutes(){
@@ -18,6 +20,7 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
     'slider': (BuildContext context) => SliderPage(),
+    'list': (BuildContext context) => ListaPage(),
 };
 
 }
